5a7e37717685dbde5480354f36ffa47a8380a128
tshafiq99 <tariqs@boxdgaming.com>
2025-10-13 10:39:26 -0400

    updating schema

--- schema/users.sql
+++
@@ -1,8 +0,0 @@
--- USERS
-create table users (
-  id uuid primary key default gen_random_uuid(),
-  first_name text not null,
-  last_name  text not null,
-  email text unique not null,
-  created_at timestamptz default now()
-);
